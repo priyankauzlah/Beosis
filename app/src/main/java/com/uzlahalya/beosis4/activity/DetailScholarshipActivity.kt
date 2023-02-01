227f59cac705c30af13a5dc0f18ba447faa5f57b
priyanka uzlah <priyankauzlah@gmail.com>
2023-02-01 14:58:13 +0700

    Detail Scholarship (manual data) unfixed layout

---
+++ app/src/main/java/com/uzlahalya/beosis4/activity/DetailScholarshipActivity.kt
@@ -0,0 +1,31 @@
+package com.uzlahalya.beosis4.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uzlahalya.beosis4.R
+import com.uzlahalya.beosis4.databinding.ActivityDetailScholarshipBinding
+import kotlinx.android.synthetic.main.activity_detail_scholarship.*
+
+class DetailScholarshipActivity : AppCompatActivity() {
+
+    lateinit var detailScholarshipBinding : ActivityDetailScholarshipBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        detailScholarshipBinding = ActivityDetailScholarshipBinding.inflate(layoutInflater)
+        setContentView(detailScholarshipBinding.root)
+        supportActionBar?.hide()
+        ib_arrow_back_detailscholarship.setOnClickListener { startActivity(Intent(MainActivity.getLaunchService(this))) }
+
+        getIntent().getStringExtra("SCHOLARSHIPLOGO")?.let { detailScholarshipBinding.ivImageDetailscholarship.setImageResource(it.toInt()) }
+        detailScholarshipBinding.tvTitleScholarshipDetailscholarship.text = getIntent().getStringExtra("SCHOLARSHIPNAME")
+        detailScholarshipBinding.tvUniDetailscholarship.text = getIntent().getStringExtra("SCHOLARSHIPUNIVERSITY")
+        detailScholarshipBinding.tvCountryDetailscholarship.text = getIntent().getStringExtra("SCHOLARSHIPCOUNTRY")
+        detailScholarshipBinding.tvMajorDetailscholarship.text = getIntent().getStringExtra("SCHOLARSHIPMAJOR")
+        detailScholarshipBinding.tvDegreeDetailScholarship.text = getIntent().getStringExtra("SCHOLARSHIPDEGREE")
+        detailScholarshipBinding.tvOpenregistrationDetailscholarship.text = getIntent().getStringExtra("SCHOLARSHIPOPENREGISTRATION")
+        detailScholarshipBinding.tvDeadlineDetailscholarship.text = getIntent().getStringExtra("SCHOLARSHIPCLOSEREGRISTRATION")
+
+    }
+}
