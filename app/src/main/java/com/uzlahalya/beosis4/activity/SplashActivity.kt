0e96ed2b31da32a2a88b9e6d4b5e88f7c814c83b
priyanka uzlah <priyankauzlah@gmail.com>
2023-02-01 14:53:34 +0700

    Animated Splash Screen

--- app/src/main/java/com/uzlahalya/beosis4/activity/SplashActivity.kt
+++ app/src/main/java/com/uzlahalya/beosis4/activity/SplashActivity.kt
@@ -1,8 +1,8 @@
 package com.uzlahalya.beosis4.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
 import com.uzlahalya.beosis4.R
 
